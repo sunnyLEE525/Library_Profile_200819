8dda757ff6c8c327d559ef6257e8b19c5e33c270
이선영 <sunny_525@naver.com>
2020-08-19 20:56:28 +0900

    Glide 라이브러리로 웹 이미지 출력

--- app/src/main/java/kr/co/tjoeun/library_profile_200819/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/library_profile_200819/MainActivity.kt
@@ -2,6 +2,8 @@ package kr.co.tjoeun.library_profile_200819
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
@@ -24,6 +26,10 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+//        Glide로 웹상의 차은우 사진을 프사로 반영
+        Glide.with(mContext).load("https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/170924_%ED%91%B8%EB%93%9C%ED%8A%B8%EB%9F%AD%EC%9D%B4%EB%B2%A4%ED%8A%B8.jpg/250px-170924_%ED%91%B8%EB%93%9C%ED%8A%B8%EB%9F%AD%EC%9D%B4%EB%B2%A4%ED%8A%B8.jpg").into(profilePhotoImg)
+
 
     }
 
