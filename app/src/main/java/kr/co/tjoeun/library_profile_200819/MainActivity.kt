32f2dde220eda50f023e9794460c52cca76db538
이선영 <sunny_525@naver.com>
2020-08-19 20:01:11 +0900

    프로필 사진을 누르면 크게 보기 화면으로 들어오기-사진첨부 아직 안함

--- app/src/main/java/kr/co/tjoeun/library_profile_200819/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/library_profile_200819/MainActivity.kt
@@ -1,13 +1,25 @@
 package kr.co.tjoeun.library_profile_200819
 
+import android.content.Intent
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setUpEvents()
+        setValues()
 }
     override fun setUpEvents() {
+
+        profilePhotoImg.setOnClickListener {
+//            사진을 크게
+
+            val myIntent = Intent(mContext, ViewProfilePhotoActivity::class.java)
+            startActivity(myIntent)
+
+        }
 
     }
 
---
+++ app/src/main/java/kr/co/tjoeun/library_profile_200819/ViewProfilePhotoActivity.kt
@@ -0,0 +1,22 @@
+package kr.co.tjoeun.library_profile_200819
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewProfilePhotoActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_profile_photo)
+        setUpEvents()
+        setValues()
+}
+    override fun setUpEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+
+}
